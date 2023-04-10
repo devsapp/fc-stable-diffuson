06b8a60ed2f3523e895043c69dba42ed517ea5a7
hanxie <hanxie.wq@alibaba-inc.com>
2023-04-10 19:14:44 +0800

    优化landing页面,尝试增加提前触达启动服务

--- src/code/src/PreLoad.js
+++ src/code/src/PreLoad.js
@@ -1,11 +1,30 @@
 
+import { useEffect, useState } from "react";
+
+
 
 export default function PreLoad() {
+  const [words, setWords] = useState('');
+  const data = '本次体验所需镜像较大,函数计算需要进行冷启动，请耐心等待2-5分钟'.split('')
+  let index = 0;
+  let typeWords = ''
+  function writing(index) {
+    if (index < data.length) {
+      typeWords += data[index]
+      setWords(typeWords)
+      setTimeout(writing.bind(this), 300, ++index)
+    }
+  }
+  useEffect(() => {
+    writing(index);
+  }, []);
   return (
     <div className="loading">
-      <h1>ServerlessDevs欢迎你</h1>
-      <div className="image-placeholder"></div> 
-      <p className="loading-text">程序正在加载中，请稍后</p>
+
+      <img src="https://img.alicdn.com/imgextra/i4/O1CN018H1Ebb1MWTxEtS0tT_!!6000000001442-2-tps-1920-1080.png" style={{ width: 1200, margin: '95px auto', display: 'block' }} />
+      <div id="tips">
+        {words}
+      </div>
     </div>
   );
 }
